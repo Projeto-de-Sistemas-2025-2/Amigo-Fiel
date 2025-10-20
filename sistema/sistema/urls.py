8dd6dd8b7372dd3e739078d82a614fbc7372d27b
Henrique Wendler <henriquewendlerserra@gmail.com>
2025-10-20 10:49:17 -0300

    Revert "Debug logout links e buttons"

--- sistema/sistema/urls.py
+++ sistema/sistema/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
 
     # Auth
     path('login/',  auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', amigofiel_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 
     # Cadastro
     path('cadastro/', amigofiel_views.cadastro, name='cadastro'),
