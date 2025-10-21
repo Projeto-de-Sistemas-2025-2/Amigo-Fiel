bd13102e1937dda2246e6335ca5eb8e067887782
Kiwi <eduardohenriquerramos@gmail.com>
2025-10-21 16:00:23 -0300

    Fix logout view import and enhance scroll behavior in home template

--- sistema/sistema/urls.py
+++ sistema/sistema/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from AmigoFiel import views as amigofiel_views
+from django.contrib.auth.views import LogoutView
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -13,7 +14,7 @@ urlpatterns = [
 
     # Auth
     path('login/',  auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', amigofiel_views.LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     # Cadastro
     path('cadastro/', amigofiel_views.cadastro, name='cadastro'),
