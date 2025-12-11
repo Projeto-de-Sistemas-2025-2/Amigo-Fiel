74a477d52746491f5d1a9e6eb0d5ab2b4f5cc92f
Kiwi <eduardohenriquerramos@gmail.com>
2025-12-11 19:54:07 -0300

    landing-page

--- sistema/AmigoFiel/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AmigofielConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AmigoFiel'
--- sistema/AmigoFiel/context_processors.py
+++
@@ -1,8 +0,0 @@
-# AmigoFiel/context_processors.py
-def perfis_flags(request):
-    u = request.user
-    return {
-        "eh_empresa": hasattr(u, "perfil_empresa"),
-        "eh_comum": hasattr(u, "perfil_comum"),
-        "eh_ong": hasattr(u, "perfil_ong"),
-    }
